1177c18ab5c97c2d6d8be6fe936a2bf4b052f748
Romain Bailhache <romain.bailhache@gmail.com>
2022-12-15 10:57:36 +0100

    Update Header.js

--- rick_morty/src/components/Header.js
+++ rick_morty/src/components/Header.js
@@ -1,6 +1,8 @@
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png'
 import '../styles/header.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faHtml5, faCss3Alt, faJs, faReact } from '@fortawesome/free-brands-svg-icons'
 
 import React from 'react'
 
@@ -8,7 +10,12 @@ const Header = () => {
   return (
     
     <header>
-      <img src={logo}/>
+      <NavLink to="/home"><img src={logo}/></NavLink>
+      <p>Rick et Morty : apprentissage de react sans prise de tête</p>
+        <FontAwesomeIcon icon={faHtml5} color='orangered' className='icon-header'/>
+        <FontAwesomeIcon icon={faCss3Alt} color='blue' className='icon-header'/>
+        <FontAwesomeIcon icon={faJs} color='yellow' className='icon-header'/>
+        <FontAwesomeIcon icon={faReact} color='cyan' className='icon-header'/>
     </header>
     
     
@@ -16,3 +23,4 @@ const Header = () => {
 }
 
 export default Header
+
