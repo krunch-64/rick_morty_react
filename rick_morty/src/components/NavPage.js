32b42569921817f33fb59bbe6ae1cb56ef66df8b
Romain Bailhache <romain.bailhache@gmail.com>
2022-12-15 13:13:49 +0100

    push

--- rick_morty/src/components/NavPage.js
+++ rick_morty/src/components/NavPage.js
@@ -5,7 +5,6 @@ const NavPage = (props) => {
   return (
     <div className='navpage'>
       {props.page == 1 ? <p/>:<button onClick={() => props.setPage(props.page - 1)} className='nav-btn'>Page {props.page-1}</button>}
-      
       <p>Page: {props.page}</p>
       <button onClick={() => props.setPage(props.page + 1)} className='nav-btn'>Page {props.page+1}</button>
     </div>
--- rick_morty/src/pages/Home.js
+++ rick_morty/src/pages/Home.js
@@ -4,9 +4,6 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const Home = () => {
-
-
-
   return (
     <div>
         <Header/>
